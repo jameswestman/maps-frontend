aebed03dababd5b19a72b3f2e4dedee552ea374b
James Westman <james@jwestman.net>
2022-10-02 16:27:19 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Map } from "maplibre-gl";
+  import { onDestroy, onMount } from "svelte";
+  import 'maplibre-gl/dist/maplibre-gl.css';
+
+  let map: Map;
+  let mapContainer: HTMLElement;
+
+  onMount(() => {
+    map = new Map({
+      container: mapContainer,
+      style: "https://tiles.maps.jwestman.net/styles/basic-preview/style.json",
+    });
+  });
+
+  onDestroy(() => {
+    map.remove();
+  });
+</script>
+
+<div class="map" id="map" bind:this={mapContainer} />
+
+<style>
+  .map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+</style>
