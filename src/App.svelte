275fada82c313f616b51f9a7a10529f563912079
James Westman <james@jwestman.net>
2023-01-01 16:30:26 -0600

    Code style

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,9 @@
 
   const setVariant = (newVariant: string) => {
     variant = newVariant;
-    map.setStyle(`https://tiles.maps.jwestman.net/styles/${variant}/style.json`);
+    map.setStyle(
+      `https://tiles.maps.jwestman.net/styles/${variant}/style.json`
+    );
   };
 
   onMount(() => {
@@ -40,12 +42,25 @@
 <div>
   <div class="map" id="map" bind:this={mapContainer} />
 
-  <div class="container position-absolute top-0 left-0 right-0" style="pointer-events: none">
+  <div
+    class="container position-absolute top-0 left-0 right-0"
+    style="pointer-events: none"
+  >
     <div class="row" style="pointer-events: none">
       <div class="col-12 col-md-6 col-lg-4 p-3" style="pointer-events: auto">
         <ButtonGroup class="w-100">
-          <Button active={variant === "light"} on:click={() => setVariant("light")}>Light</Button>
-          <Button active={variant === "dark"} on:click={() => setVariant("dark")}>Dark</Button>
+          <Button
+            active={variant === "light"}
+            on:click={() => setVariant("light")}
+          >
+            Light
+          </Button>
+          <Button
+            active={variant === "dark"}
+            on:click={() => setVariant("dark")}
+          >
+            Dark
+          </Button>
         </ButtonGroup>
 
         <Alert class="mt-3" color="warning" dismissible>
@@ -65,15 +80,20 @@
         <Alert class="mt-3" color="success" dismissible>
           <h5>You can sponsor my work on GitHub Sponsors or Patreon!</h5>
           <p>
-            Your support helps me pay
-            hosting costs for the map server, and encourages me to devote more
-            time to improving this map style and other projects.
+            Your support helps me pay hosting costs for the map server, and
+            encourages me to devote more time to improving this map style and
+            other projects.
           </p>
           <div>
-            <Button href="https://github.com/sponsors/jameswestman" color="primary">
+            <Button
+              href="https://github.com/sponsors/jameswestman"
+              color="primary"
+            >
               GitHub Sponsors
             </Button>
-            <Button href="https://patreon.com/jwestman" color="primary">Patreon</Button>
+            <Button href="https://patreon.com/jwestman" color="primary">
+              Patreon
+            </Button>
           </div>
         </Alert>
       </div>
