45034b982bbcf30514a96070dd35b69fed22ab4d
James Westman <james@jwestman.net>
2023-11-20 18:59:09 -0600

    Add icons from the map style

--- scripts/generate_sprites.js
+++ scripts/generate_sprites.js
@@ -4,11 +4,11 @@ import path from "node:path";
 import glob from "glob";
 import { Sprites } from "@basemaps/sprites";
 
-await fs.mkdir("./dist/sprites/", { recursive: true });
+await fs.mkdir("./public/sprites/", { recursive: true });
 
-const sprites = await Promise.all(
+const shields = await Promise.all(
   (
-    await glob("./icons/*.svg")
+    await glob("./src/thirdparty/openstreetmap-americana/icons/*.svg")
   ).map(async (spritePath) => {
     const id = path.parse(spritePath).name;
     const buffer = await fs.readFile(spritePath);
@@ -16,14 +16,30 @@ const sprites = await Promise.all(
   })
 );
 
+const icons = await Promise.all(
+    (await glob("./src/thirdparty/map-style/icons/*.svg")).map(async (spritePath) => {
+        const id = path.parse(spritePath).name;
+        const buffer = await fs.readFile(spritePath);
+        return { id, buffer, sdf: id.endsWith('-symbolic') };
+    })
+);
+
+const sprites = [...shields, ...icons];
+
 console.log(`Building ${sprites.length} sprites`);
 
 const generated = await Sprites.generate(sprites, [1, 2]);
 
 for (const result of generated) {
   const scaleText = result.pixelRatio === 1 ? "" : `@${result.pixelRatio}x`;
-  const outputPng = `./dist/sprites/sprite${scaleText}.png`;
-  const outputJson = `./dist/sprites/sprite${scaleText}.json`;
+  const outputPng = `./public/sprites/sprite${scaleText}.png`;
+  const outputJson = `./public/sprites/sprite${scaleText}.json`;
+
+  for (const sprite of sprites) {
+    if (sprite.sdf) {
+        result.layout[sprite.id].sdf = true;
+    }
+  }
 
   await fs.writeFile(outputPng, result.buffer);
   await fs.writeFile(outputJson, JSON.stringify(result.layout, null, 2));
