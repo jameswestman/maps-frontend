6c60651eb6a9931303d348c7c5cdbc1515155f56
James Westman <james@jwestman.net>
2023-11-25 12:42:40 -0600

    Continue fixing icons

--- scripts/generate_sprites.js
+++ scripts/generate_sprites.js
@@ -6,7 +6,7 @@ import { Sprites } from "@basemaps/sprites";
 
 await fs.mkdir("./public/sprites/", { recursive: true });
 
-const shields = await Promise.all(
+const sprites = await Promise.all(
   (
     await glob("./src/thirdparty/openstreetmap-americana/icons/*.svg")
   ).map(async (spritePath) => {
@@ -16,16 +16,6 @@ const shields = await Promise.all(
   })
 );
 
-const icons = await Promise.all(
-    (await glob("./src/thirdparty/map-style/icons/*.svg")).map(async (spritePath) => {
-        const id = path.parse(spritePath).name;
-        const buffer = await fs.readFile(spritePath);
-        return { id, buffer, sdf: id.endsWith('-symbolic') };
-    })
-);
-
-const sprites = [...shields, ...icons];
-
 console.log(`Building ${sprites.length} sprites`);
 
 const generated = await Sprites.generate(sprites, [1, 2]);
@@ -34,12 +24,6 @@ for (const result of generated) {
   const scaleText = result.pixelRatio === 1 ? "" : `@${result.pixelRatio}x`;
   const outputPng = `./public/sprites/sprite${scaleText}.png`;
   const outputJson = `./public/sprites/sprite${scaleText}.json`;
-
-  for (const sprite of sprites) {
-    if (sprite.sdf) {
-        result.layout[sprite.id].sdf = true;
-    }
-  }
 
   await fs.writeFile(outputPng, result.buffer);
   await fs.writeFile(outputJson, JSON.stringify(result.layout, null, 2));
