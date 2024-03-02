1248cf864e25d46c3721bf55f7c289a44c211dc1
James Westman <james@jwestman.net>
2024-03-02 15:04:02 -0600

    inspector: Fix crash

--- src/components/InspectorCard.svelte
+++ src/components/InspectorCard.svelte
@@ -3,43 +3,46 @@
   import { inspectedFeatures } from "../inspector";
 </script>
 
-<Card>
-  <div class="card-header d-flex justify-content-between">
-    <span class="tnum">
-      {$inspectedFeatures.coords[0].toFixed(5)},
-      {$inspectedFeatures.coords[1].toFixed(5)}
-    </span>
+{#if $inspectedFeatures.coords}
+  <Card>
+    <div class="card-header d-flex justify-content-between">
+      <span class="tnum">
+        {$inspectedFeatures.coords[0].toFixed(5)},
+        {$inspectedFeatures.coords[1].toFixed(5)}
+      </span>
 
-    {#if $inspectedFeatures.clicked}
-      <Button
-        close
-        class="align-middle"
-        on:click={() => ($inspectedFeatures = { features: [], clicked: false })}
-      />
+      {#if $inspectedFeatures.clicked}
+        <Button
+          close
+          class="align-middle"
+          on:click={() =>
+            ($inspectedFeatures = { features: [], clicked: false })}
+        />
+      {/if}
+    </div>
+    {#if $inspectedFeatures.features.length > 0}
+      <ListGroup flush>
+        {#each $inspectedFeatures.features as feature}
+          <ListGroupItem>
+            <strong>#{feature.sourceLayer}</strong>
+            <small class="text-muted">{feature.id}</small>
+
+            <table class="table table-sm">
+              <tbody>
+                {#each Object.entries(feature.properties).sort() as [key, value]}
+                  <tr>
+                    <th>{key}</th>
+                    <td>{value}</td>
+                  </tr>
+                {/each}
+              </tbody>
+            </table>
+          </ListGroupItem>
+        {/each}
+      </ListGroup>
     {/if}
-  </div>
-  {#if $inspectedFeatures.features.length > 0}
-    <ListGroup flush>
-      {#each $inspectedFeatures.features as feature}
-        <ListGroupItem>
-          <strong>#{feature.sourceLayer}</strong>
-          <small class="text-muted">{feature.id}</small>
-
-          <table class="table table-sm">
-            <tbody>
-              {#each Object.entries(feature.properties).sort() as [key, value]}
-                <tr>
-                  <th>{key}</th>
-                  <td>{value}</td>
-                </tr>
-              {/each}
-            </tbody>
-          </table>
-        </ListGroupItem>
-      {/each}
-    </ListGroup>
-  {/if}
-</Card>
+  </Card>
+{/if}
 
 <style>
   .card-header:last-child {
