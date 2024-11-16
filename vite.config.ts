fd73b0be5a39d86240fe1697a1b914249ba79d27
James Westman <james@jwestman.net>
2024-11-16 10:02:06 -0600

    Add basic PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { execSync } from "child_process";
+import { VitePWA } from "vite-plugin-pwa";
 
 const commitHash = execSync("git rev-parse HEAD").toString().trim();
 
@@ -9,10 +10,21 @@ export default defineConfig({
   define: {
     __COMMIT_HASH__: JSON.stringify(commitHash),
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      manifest: {
+        name: "Maps",
+        theme_color: "#f904a4",
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   build: {
     /* index.js is going to be big, not much we can do about it, but do
        still complain if it gets much bigger */
     chunkSizeWarningLimit: 1500,
-  }
+  },
 });
